5f195c00fc288893ae871d84a23d35d4f1ff0e48
username000101 <cxx.username000101@gmail.com>
2025-11-28 01:37:54 +0100

    Remove test code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 ﻿#include "Logger/Logger.h"
 #include "Main/Loop.h"
 #include "Safety/Safety.h"
+
 int main()
 {
-	sub::logger::InitializeLogger();
-	sub::safety::Safety::EnableSafety(nullptr);
-	int* x = nullptr;
-	std::printf("%d\n", *x);
+	/* Nothing yet */ 
 }
