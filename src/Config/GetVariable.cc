52f34678e9bc3bb17708e12f9f9909846e0047e6
username000101 <cxx.username000101@gmail.com>
2025-12-02 03:21:09 +0300

    Remove logging

--- src/Config/GetVariable.cc
+++ src/Config/GetVariable.cc
@@ -4,6 +4,11 @@
 
 std::optional<std::pair<std::string, std::string>> sub::config::Configuration::GetVariable(const std::string& name) {
 	if (!config.loadable_config.has_value()) {
-		LOGW << "Trying to add variable '" << name << "' without an active configuration" << std::endl;
+		LOGW << "Trying to get variable '" << name << "' without an active configuration" << std::endl;
+		return std::nullopt;
 	}
+
+	if (!config.loadable_config.value().variables.contains(name)) return std::nullopt;
+
+	return std::make_pair(name, config.loadable_config.value().variables.at(name));
 }
