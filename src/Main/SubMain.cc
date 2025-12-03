f61bb63431d42e9cf3c1f2364279af46abc7a53b
username000101 <cxx.username000101@gmail.com>
2025-12-03 15:35:56 +0300

    Call the sub::safety::Safety::Shutdown if the sub::config::Configuration::Load returns false

--- src/Main/SubMain.cc
+++ src/Main/SubMain.cc
@@ -1,12 +1,16 @@
 ﻿#include "Config/Config.h"
 #include "Logger/Logger.h"
 #include "Loop.h"
-#include "Utils/Macros.h"
+#include "Safety/Safety.h"
 #include "Server/RPC/RPC.h"
 #include "Td/Td.h"
+#include "Utils/Macros.h"
 
 void sub::main::sub_main(const std::filesystem::path& cfgfile, std::optional<std::string> spoofed_version) {
-	config::Configuration::Load(cfgfile);
+	if (!config::Configuration::Load(cfgfile)) {
+		LOGF << "Failed to init SUB v" << SUB_GET_VERSION() << std::endl;
+		safety::Safety::Shutdown(EXIT_FAILURE, "Check previous logs");
+	}
 	auto cfg = config::Configuration::Get();
 	cfg.spoofed_version = spoofed_version;
 
