a8ea4962b14e7f479df861d60e31289ff5c879b7
username000101 <cxx.username000101@gmail.com>
2025-12-03 17:50:52 +0300

    Configuration: Fix segmentation fault in the sub::config::Configuration::RunProcesses function Main: Call the sub::config::Configuration::RunProcesses function

--- src/Config/RunProcesses.cc
+++ src/Config/RunProcesses.cc
@@ -9,6 +9,13 @@ void sub::config::Configuration::RunProcesses() {
 		return;
 	}
 
+	if (config.loadable_config->modules.empty()) {
+		LOGW << "Modules array is empty" << std::endl;
+		return;
+	}
+
+	auto processes = std::make_shared<std::vector<std::unique_ptr<subprocess::Popen>>>();
+
 	for (auto& module: config.loadable_config->modules) {
 		if (!std::filesystem::exists(module.file)) {
 			LOGE << "Failed to run module '" << module.file.string() << "': file doesn't exist" << std::endl;
@@ -17,10 +24,17 @@ void sub::config::Configuration::RunProcesses() {
 
 		if (module.prefix.has_value()) {
 			LOGD << "Running module '" << module.file.string() << "' with these parameters(prefix): '" << module.prefix.value() << "'" << std::endl;
-			config.processes->emplace_back(std::make_unique<subprocess::Popen>(subprocess::RunBuilder({module.prefix.value(), module.file.string()}).popen()));
+			auto popen_ptr = std::make_unique<subprocess::Popen>(subprocess::RunBuilder({module.prefix.value(), module.file.string()}).popen());
+			if (!popen_ptr) {
+				LOGE << "Failed to create popen ptr for module '" << module.file.string() << "'" << std::endl;
+				continue;
+			}
+			processes->emplace_back(std::move(popen_ptr));
 		} else {
 			LOGD << "Running module '" << module.file.string() << "' without parameters(prefix)" << std::endl;
-			config.processes->emplace_back(std::make_unique<subprocess::Popen>(subprocess::RunBuilder({module.file.string()}).popen()));
+			processes->emplace_back(std::make_unique<subprocess::Popen>(subprocess::RunBuilder({module.file.string()}).popen()));
 		}
 	}
+	LOGI << "Loaded " << processes->size() << " processes" << std::endl;
+	config.processes = std::move(processes);
 }
